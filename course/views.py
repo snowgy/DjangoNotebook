f2a70fc2cefbf46766c32f5d5d8b57c839e62b18
gongyue <11611908@mail.sustc.edu.cn>
2017-02-19 14:36:37 +0800

    complete base and home

--- course/views.py
+++ course/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from course.models import Homework, Course
 # Create your views here.
+
+
+def home(request):
+    course_list = Course.objects.all()
+    return render(request, 'home.html', {'course_list': course_list})
+
+
+def homework(request):
+    course = Course.objects.all()
+    homework_list = Homework.objects.all()
+    return render(request, 'homework.html', {'homework_list': homework_list})
+
+
+def course_detail(request):
+    return render(request, 'base.html')
+
+
--- notebook/urls.py
+++ notebook/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from course import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
+    url(r'^homework$', views.homework, name='homework'),
+    url(r'^course$', views.course_detail, name='course'),
 ]
